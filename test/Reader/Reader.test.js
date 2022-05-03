db5dcbcc38f77f53e7057611f105c837ea28ef0f
MagdielAndres <magdiel.vampire@gmail.com>
2022-05-03 17:55:59 -0500

    Agrgando el test para leer el archivo JSON

---
+++ test/Reader/Reader.test.js
@@ -0,0 +1,10 @@
+const Reader =  require("../../lib/Reader/Reader");
+
+describe('Leer un archivo Estudiantes.json', () => { 
+    test('1) Obtener la cantidad de estudiantes', () => { 
+        const path = "Estudiantes.json";
+        const students = Reader.readJsonFile(path);
+
+        expect(students.length).toBe(51);
+    });
+});
